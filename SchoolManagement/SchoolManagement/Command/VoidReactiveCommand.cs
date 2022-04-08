c1b30d13b1e50f0ff7ff2b6f1446f910eaf4cc16
MamedYahyayev <yahyayev2001@gmail.com>
2022-04-08 11:44:53 +0400

    mvvm structured configured for views

---
+++ SchoolManagement/SchoolManagement/Command/VoidReactiveCommand.cs
@@ -0,0 +1,68 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Command
+{
+    public class VoidReactiveCommand : ReactiveCommand<Unit, Unit>
+    {
+        public VoidReactiveCommand(Func<Unit, IObservable<Unit>> execute, IObservable<bool> canExecute, IScheduler outputScheduler) :
+            base(execute, canExecute, outputScheduler, outputScheduler)
+        { }
+
+        public static VoidReactiveCommand Create(Action execute, IObservable<bool> canExecute = null, IScheduler outputScheduler = null)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+
+            return new VoidReactiveCommand(
+                _ => Observable.Create<Unit>(
+                    observer =>
+                    {
+                        execute();
+                        observer.OnNext(Unit.Default);
+                        observer.OnCompleted();
+                        return Disposable.Empty;
+                    }),
+                canExecute ?? Observable.Return(true),
+                outputScheduler ?? RxApp.MainThreadScheduler);
+        }
+    }
+
+    public class VoidReactiveCommand<T> : ReactiveCommand<T, Unit>
+    {
+        public VoidReactiveCommand(Func<T, IObservable<Unit>> execute, IObservable<bool> canExecute, IScheduler outputScheduler) :
+            base(execute, canExecute, outputScheduler, outputScheduler)
+        { }
+
+        public static VoidReactiveCommand<T> Create(Action<T> execute, IObservable<bool> canExecute = null, IScheduler outputScheduler = null)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+
+            return new VoidReactiveCommand<T>(
+                param => Observable.Create<Unit>(
+                    observer =>
+                    {
+                        execute(param);
+                        observer.OnNext(Unit.Default);
+                        observer.OnCompleted();
+                        return Disposable.Empty;
+                    }),
+                canExecute ?? Observable.Return(true),
+                outputScheduler ?? RxApp.MainThreadScheduler);
+        }
+    }
+
+}
---
+++ SchoolManagement/SchoolManagement/ViewModel/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.ViewModel
+{
+    public class MainViewModel : ReactiveObject
+    {
+        private object _currentView;
+
+
+        public StudentViewModel StudentViewModel { get; set; }
+        public TeacherViewModel TeacherViewModel { get; set; }
+        public SubjectViewModel SubjectViewModel { get; set; }
+
+        public Object CurrentView
+        {
+            get => _currentView;
+            set => this.RaiseAndSetIfChanged(ref _currentView, value); 
+        }
+
+        public MainViewModel()
+        {
+            StudentViewModel = new StudentViewModel();
+            TeacherViewModel = new TeacherViewModel();
+            SubjectViewModel = new SubjectViewModel();
+
+            CurrentView = StudentViewModel;
+        }
+
+    }
+}
