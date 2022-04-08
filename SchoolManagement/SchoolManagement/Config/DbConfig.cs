450f39cf2656defc5751592d1c51e56e734b8a90
MamedYahyayev <yahyayev2001@gmail.com>
2022-04-08 14:33:55 +0400

    sql server configured

---
+++ SchoolManagement/SchoolManagement/Config/DbConfig.cs
@@ -0,0 +1,23 @@
+﻿using SchoolManagement.Constant;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Config
+{
+    public class DbConfig
+    {
+        public static SqlConnection Connect() => new SqlConnection(SqlServerConnection());
+
+        private static string SqlServerConnection()
+        {
+            string connection = string.Format("Data Source={0};Initial Catalog={1};User ID={2};Password={3}",
+                SqlServerDbConfig.DATA_SOURCE, SqlServerDbConfig.DATABASE, SqlServerDbConfig.USER, SqlServerDbConfig.PASSWORD);
+
+            return connection;
+        }
+    }
+}
---
+++ SchoolManagement/SchoolManagement/Constant/SqlServerDbConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Constant
+{
+    public class SqlServerDbConfig
+    {
+        public const string DATA_SOURCE = "localhost";
+        public const string DATABASE = "SchoolManagement";
+        public const string USER = "sa";
+        public const string PASSWORD = "mamed2001";
+    }
+}
