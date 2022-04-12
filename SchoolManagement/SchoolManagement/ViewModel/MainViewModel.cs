9afd1301c7c77af28b5e5a83b77e5cd01ff9b926
MamedYahyayev <yahyayev2001@gmail.com>
2022-04-12 16:55:33 +0400

    popup created for delete operation

--- SchoolManagement/SchoolManagement/ViewModel/MainViewModel.cs
+++ SchoolManagement/SchoolManagement/ViewModel/MainViewModel.cs
@@ -17,7 +17,6 @@ namespace SchoolManagement.ViewModel
         public MainViewModel()
         {
             StudentListViewModel = new StudentListViewModel();
-            TeacherListViewModel = new TeacherListViewModel();
 
             CurrentView = StudentListViewModel;
         }
@@ -41,6 +40,13 @@ namespace SchoolManagement.ViewModel
             set => this.RaiseAndSetIfChanged(ref _searchInput, value);
         }
 
+        private bool _isPopupOpen;
+        public bool IsPopupOpen
+        {
+            get => _isPopupOpen;
+            set => this.RaiseAndSetIfChanged(ref _isPopupOpen, value);
+        }
+
         #endregion
 
         #region Functions
@@ -62,31 +68,52 @@ namespace SchoolManagement.ViewModel
 
         private void InsertPerson()
         {
-            if (CurrentView is StudentListViewModel)
+            if (IsCurrentViewStudent())
                 CurrentView = new StudentOperationViewModel(null, CurrentViewChange);
-            else if(CurrentView is TeacherListViewModel)
-                CurrentView = new TeacherOperationViewModel("INSERT", CurrentViewChange);
+            else if (IsCurrentViewTeacher())
+                CurrentView = new TeacherOperationViewModel(null, CurrentViewChange);
         }
 
         private void UpdatePerson()
         {
-            if (CurrentView is StudentListViewModel && StudentListViewModel.CurrentStudent.Id != null)
+            if (IsCurrentViewStudent() && IsStudentSelected())
                 CurrentView = new StudentOperationViewModel(StudentListViewModel.CurrentStudent.Id, CurrentViewChange);
+            else if (IsCurrentViewTeacher() && IsTeacherSelected())
+                CurrentView = new TeacherOperationViewModel(TeacherListViewModel.CurrentTeacher.Id, CurrentViewChange);
+
         }
 
         private void DeletePerson()
         {
-            if (CurrentView is StudentListViewModel && StudentListViewModel.CurrentStudent.Id != null)
+            if (IsCurrentViewStudent() && IsStudentSelected())
                 StudentListViewModel.DeleteStudent((int)StudentListViewModel.CurrentStudent.Id);
+
+            else if (IsCurrentViewTeacher() && IsTeacherSelected())
+                TeacherListViewModel.DeleteTeacher((int)TeacherListViewModel.CurrentTeacher.Id);
+
+            IsPopupOpen = false;
+
         }
 
         private void SearchPerson()
         {
-            if (CurrentView is StudentListViewModel)
-            {
+            if (IsCurrentViewStudent())
                 StudentListViewModel.SearchStudent(SearchInput);
-            }
+            else if (IsCurrentViewTeacher())
+                TeacherListViewModel.SearchTeacher(SearchInput);
         }
+
+
+        private void OpenPopup()
+        {
+            if (IsStudentSelected() || IsTeacherSelected()) IsPopupOpen = true;
+        }
+
+        private bool IsCurrentViewStudent() => CurrentView is StudentListViewModel;
+        private bool IsCurrentViewTeacher() => CurrentView is TeacherListViewModel;
+        private bool IsStudentSelected() => StudentListViewModel?.CurrentStudent?.Id != null;
+        private bool IsTeacherSelected() => TeacherListViewModel?.CurrentTeacher?.Id != null;
+
 
         #endregion
 
@@ -114,6 +141,14 @@ namespace SchoolManagement.ViewModel
         private VoidReactiveCommand _searchCommand;
         public VoidReactiveCommand SearchCommand =>
             _searchCommand ??= VoidReactiveCommand.Create(SearchPerson);
+
+        private VoidReactiveCommand _openPopupCommand;
+        public VoidReactiveCommand OpenPopupCommand =>
+            _openPopupCommand  ??= VoidReactiveCommand.Create(OpenPopup);
+
+        private VoidReactiveCommand _closePopupCommand;
+        public VoidReactiveCommand ClosePopupCommand =>
+            _closePopupCommand ??= VoidReactiveCommand.Create(() => IsPopupOpen = false);
 
         #endregion
 
