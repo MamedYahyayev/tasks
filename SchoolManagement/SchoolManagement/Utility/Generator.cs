2993d25eaa69ca8741d3824baa08e752f49e383c
MamedYahyayev <yahyayev2001@gmail.com>
2022-04-21 12:52:11 +0400

    generator utility class created for generating random id

---
+++ SchoolManagement/SchoolManagement/Utility/Generator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Utility
+{
+    public class Generator
+    {
+        public static int GenerateId() => new Random().Next(1, 100_000_000);
+    }
+}
