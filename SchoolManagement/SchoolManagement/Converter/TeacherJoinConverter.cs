cbf8bd4dd06f29414135bc61dba464e07dd50152
MamedYahyayev <yahyayev2001@gmail.com>
2022-04-25 17:04:59 +0400

    converter created for teachers joined with comma

---
+++ SchoolManagement/SchoolManagement/Converter/TeacherJoinConverter.cs
@@ -0,0 +1,35 @@
+﻿using SchoolManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SchoolManagement.Converter
+{
+    public class TeacherJoinConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+
+            var teachers = (List<Teacher>)value;
+            var teacherNames = teachers.Select(x => x.Name + " " + x.Surname).ToList();
+            var concat = string.Join(", ", teacherNames);
+
+            return concat;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        private void Concat()
+        {
+
+        }
+    }
+}
