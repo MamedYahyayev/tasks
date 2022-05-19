53c656176332cd5d3dee8d2dd08a95af29100ddb
MamedYahyayev <yahyayev2001@gmail.com>
2022-05-19 14:39:58 +0400

    services for entities created

---
+++ LookScore/LookScoreAdmin/Service/GameService.cs
@@ -0,0 +1,37 @@
+﻿using LookScoreAdmin.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookScoreAdmin.Service
+{
+    public class GameService : ICrudOperation<Game>
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Game[] FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Game FindOne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Game entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Game entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LookScore/LookScoreAdmin/Service/StatisticService.cs
@@ -0,0 +1,75 @@
+﻿namespace LookScoreAdmin.Service
+{
+    public class StatisticService
+    {
+        #region Goal Statistics
+
+        public int IncreaseGoal()
+        {
+            return 0;
+        }
+
+        public int DecreaseGoal()
+        {
+            return 0;
+        }
+
+        #endregion
+
+        #region Corner Statistics
+
+        public int IncreaseCornerKicks()
+        {
+            return 0;
+        }
+
+        public int DecreaseCornerKicks()
+        {
+            return 0;
+        }
+
+        #endregion
+
+        #region Tackles Statistics
+
+        public int IncreaseTackles()
+        {
+            return 0;
+        }
+
+        public int DecreaseTackles()
+        {
+            return 0;
+        }
+
+        #endregion
+
+        #region Pass Statistics
+
+        public int IncreasePass()
+        {
+            return 0;
+        }
+
+        public int DecreasePass()
+        {
+            return 0;
+        }
+
+        #endregion
+
+        #region Ball Possession Statistics
+
+        public int IncreaseBallPossession()
+        {
+            return 0;
+        }
+
+        public int DecreaseBallPossession()
+        {
+            return 0;
+        }
+
+        #endregion
+    }
+}
