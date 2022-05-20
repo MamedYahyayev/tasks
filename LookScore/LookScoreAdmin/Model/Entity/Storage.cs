4bd5e331cb6f3fbd7604265a4abfd8941c7520e1
MamedYahyayev <yahyayev2001@gmail.com>
2022-05-20 10:37:00 +0400

    storage created

---
+++ LookScore/LookScoreAdmin/Model/Entity/Storage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookScoreAdmin.Model.Entity
+{
+    public class Storage
+    {
+        public Club[] Clubs = new Club[0];
+        public Game[] Games = new Game[0];
+        public Player[] Players = new Player[0];
+        public GameStatistics[] GameStatistics = new GameStatistics[0];
+
+    }
+}
