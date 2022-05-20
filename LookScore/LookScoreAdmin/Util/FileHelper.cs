e1cd7a15ba3e2a59eff5b25437bda7d5db34caa2
MamedYahyayev <yahyayev2001@gmail.com>
2022-05-20 11:51:05 +0400

    utility class created for working with files

---
+++ LookScore/LookScoreAdmin/Util/FileHelper.cs
@@ -0,0 +1,28 @@
+﻿using LookScoreAdmin.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookScoreAdmin.Util
+{
+    public class FileHelper
+    {
+        private static string STORAGE_FILE_PATH = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\Data\";
+
+        public static string GetOrCreateFile(FileType fileType)
+        {
+            var path = STORAGE_FILE_PATH + "storage." + fileType.ToString().ToLower();
+            if (File.Exists(path))
+            {
+                return path;
+            }
+
+            File.Create(path);
+
+            return path;
+        }
+    }
+}
