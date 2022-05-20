8722f199ba6a30efd57cee335805e03c676f3bfe
MamedYahyayev <yahyayev2001@gmail.com>
2022-05-20 14:28:08 +0400

    short name field added into club entity

--- LookScore/LookScoreAdmin/Model/Entity/Club.cs
+++ LookScore/LookScoreAdmin/Model/Entity/Club.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LookScoreAdmin.Model.Entity
 {
@@ -11,6 +7,7 @@ namespace LookScoreAdmin.Model.Entity
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string ShortName { get; set; }
         public int PlayersCount { get; set; }
         public string LogoUrl { get; set; }
         public string Country { get; set; }
