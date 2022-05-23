8061fc4c9b0e7493baa54bed0df7d68d756a825a
MamedYahyayev <yahyayev2001@gmail.com>
2022-05-23 14:34:23 +0400

    new converted added for connecting club name and short name together

---
+++ LookScore/LookScoreAdmin/Converter/ClubJoinConverter.cs
@@ -0,0 +1,27 @@
+﻿using LookScoreAdmin.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace LookScoreAdmin.Converter
+{
+    public class ClubJoinConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return value;
+
+            Club[] clubs = (Club[])value;
+            return clubs.Select(c => c.Name + "(" + c.Name.Substring(0, 3).ToUpper() + ")").ToArray();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
