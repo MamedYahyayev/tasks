55f5aa8ee068afd5c862fa92dcff1b7aae1f1a41
MamedYahyayev <yahyayev2001@gmail.com>
2022-05-23 11:30:09 +0400

    club editor view linked to club view

--- LookScore/LookScoreAdmin/ViewModel/SubViewModel/ClubViewModel.cs
+++ LookScore/LookScoreAdmin/ViewModel/SubViewModel/ClubViewModel.cs
@@ -1,6 +1,56 @@
-﻿namespace LookScoreAdmin.ViewModel.SubViewModel
+﻿using LookScoreAdmin.Command;
+using LookScoreAdmin.Model.Entity;
+using LookScoreAdmin.Service.EntityServices;
+using ReactiveUI;
+
+namespace LookScoreAdmin.ViewModel.SubViewModel
 {
     public class ClubViewModel : BaseViewModel
     {
+        #region Private Properties
+
+        private Club[] _clubs;
+        private readonly ClubService _clubService = new ClubService();
+
+        #endregion
+
+
+        public ClubViewModel()
+        {
+            Clubs = _clubService.FindAll();
+        }
+
+        #region Public Properties
+
+        public Club[] Clubs
+        {
+            get => _clubs;
+            set => this.RaiseAndSetIfChanged(ref _clubs, value);
+        }
+
+        #endregion
+
+
+        #region Functions
+
+
+        private void OpenNewClubView()
+        {
+            MainViewModel.Instance.SetCurrentViewModel(new ClubEditorViewModel());
+        }
+
+        #endregion
+
+
+        #region Commands
+
+        private readonly VoidReactiveCommand _newClubViewCommand;
+        public VoidReactiveCommand NewClubCommand =>
+            _newClubViewCommand ?? VoidReactiveCommand.Create(OpenNewClubView);
+
+
+
+        #endregion
+
     }
 }
