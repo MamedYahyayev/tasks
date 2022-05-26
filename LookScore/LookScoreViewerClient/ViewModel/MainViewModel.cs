c8746bad16e411e3a8217f327df18cc84325c611
MamedYahyayev <yahyayev2001@gmail.com>
2022-05-26 15:11:39 +0400

    scoreboard created and designed

--- LookScore/LookScoreViewerClient/ViewModel/MainViewModel.cs
+++ LookScore/LookScoreViewerClient/ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using LookScoreServer.Service.WCFServices;
+﻿using LookScoreServer.Model.Entity;
+using LookScoreServer.Service.WCFServices;
 using ReactiveUI;
 using System.ServiceModel;
 
@@ -8,25 +9,31 @@ namespace LookScoreViewerClient.ViewModel
     {
         #region Private Properties
 
-        private string[] _games;
+        private Game[] _games;
+        private Game _selectedGame; 
 
         #endregion
 
         public MainViewModel()
         {
-
             ChannelFactory<IGameService> channelFactory = new ChannelFactory<IGameService>("GameService");
             IGameService gameService = channelFactory.CreateChannel();
 
-            Games = gameService.GetAllGamesTitle();
+            Games = gameService.FindAllGameDetails();
         }
 
         #region Public Properties
 
-        public string[] Games
+        public Game[] Games
         {
             get => _games;
             set => this.RaiseAndSetIfChanged(ref _games, value);
+        }
+
+        public Game SelectedGame
+        {
+            get => _selectedGame;
+            set => this.RaiseAndSetIfChanged(ref _selectedGame, value);
         }
 
         #endregion
