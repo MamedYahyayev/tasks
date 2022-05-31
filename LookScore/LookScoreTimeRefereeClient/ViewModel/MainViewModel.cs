784de519ca3bec418538d53d2723d1063de2d8ba
MamedYahyayev <yahyayev2001@gmail.com>
2022-05-31 11:44:13 +0400

    games fetched from server

--- LookScore/LookScoreTimeRefereeClient/ViewModel/MainViewModel.cs
+++ LookScore/LookScoreTimeRefereeClient/ViewModel/MainViewModel.cs
@@ -1,7 +1,10 @@
-﻿using ReactiveUI;
+﻿using LookScoreServer.Model.Entity;
+using LookScoreServer.Service.WCFServices;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,15 +14,33 @@ namespace LookScoreTimeRefereeClient.ViewModel
     {
         #region Private Properties
 
+        private Game[] _games;
+        private Game _selectedGame;
 
         #endregion
 
         public MainViewModel()
         {
+            ChannelFactory<IGameService> channelFactory = new ChannelFactory<IGameService>("GameService");
+            IGameService gameService = channelFactory.CreateChannel();
+
+            Games = gameService.FindAllGameDetails();
 
         }
 
         #region Public Properties
+
+        public Game[] Games
+        {
+            get => _games;
+            set => this.RaiseAndSetIfChanged(ref _games, value);
+        }
+
+        public Game SelectedGame
+        {
+            get => _selectedGame;
+            set => this.RaiseAndSetIfChanged(ref _selectedGame, value);
+        }
 
 
         #endregion
