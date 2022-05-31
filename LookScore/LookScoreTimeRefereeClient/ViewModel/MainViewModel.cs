f569c2821b3fb209380be6a0796656dd2feeb961
MamedYahyayev <yahyayev2001@gmail.com>
2022-05-31 11:33:57 +0400

    time referee client design ui completed

---
+++ LookScore/LookScoreTimeRefereeClient/ViewModel/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookScoreTimeRefereeClient.ViewModel
+{
+    public class MainViewModel : ReactiveObject
+    {
+        #region Private Properties
+
+
+        #endregion
+
+        public MainViewModel()
+        {
+
+        }
+
+        #region Public Properties
+
+
+        #endregion
+
+
+        #region Functions
+
+
+        #endregion
+
+        #region Commands
+
+
+
+        #endregion
+
+    }
+}
