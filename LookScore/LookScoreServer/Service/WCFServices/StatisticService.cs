1a5944be0b9482a46e101186d7fa9f3efe7e0a65
MamedYahyayev <yahyayev2001@gmail.com>
2022-05-31 16:09:05 +0400

    callback service created for sending request to the clients

--- LookScore/LookScoreServer/Service/WCFServices/StatisticService.cs
+++ LookScore/LookScoreServer/Service/WCFServices/StatisticService.cs
@@ -9,9 +9,11 @@ using System.Text;
 
 namespace LookScoreServer.Service.WCFServices
 {
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
     public class StatisticService : IStatisticService
     {
         private readonly EntityServices.StatisticService _statisticService;
+        private static readonly List<IStatisticCallbackService> _statisticCallbackServiceList = new List<IStatisticCallbackService>();
 
         public StatisticService()
         {
@@ -21,11 +23,27 @@ namespace LookScoreServer.Service.WCFServices
         public void ChangeStatistic(GameStatistics statistics)
         {
             _statisticService.ChangeStatistic(statistics);
+
+            Action<IStatisticCallbackService> invoke = callback => callback.NotifyGoalScored(statistics);
+            _statisticCallbackServiceList.ForEach(invoke);
+
         }
 
         public GameStatistics FindGameStatistics(int gameId)
         {
             return _statisticService.FindGameStatistics(gameId);
         }
+
+        public void JoinToChannel()
+        {
+            IStatisticCallbackService registeredClient = OperationContext.Current.GetCallbackChannel<IStatisticCallbackService>();
+
+            if (!_statisticCallbackServiceList.Contains(registeredClient))
+            {
+                _statisticCallbackServiceList.Add(registeredClient);
+            }
+
+        }
+
     }
 }
