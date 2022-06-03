49ff216e023d107c508853acd66fd77dbc57a322
MamedYahyayev <yahyayev2001@gmail.com>
2022-06-03 10:58:12 +0400

    converter created for changing seconds into timer format

---
+++ LookScore/LookScoreCommon/Utils/TimerUtil.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookScoreCommon.Utils
+{
+    public class TimerUtil
+    {
+        private TimerUtil()
+        {
+
+        }
+
+        private static string ConvertToTimePart(int seconds)
+        {
+            if(seconds.ToString().Length == 1)
+            {
+                return "0" + seconds.ToString();
+            }
+
+            return seconds.ToString();
+        }
+
+        public static string Convert(int seconds)
+        {
+            int minute = seconds / 60;
+            int remainderSeconds = seconds - (minute * 60);
+
+            string minuteStr = ConvertToTimePart(minute);
+            string secondStr = ConvertToTimePart(remainderSeconds);
+
+            return minuteStr + " : " + secondStr;
+        }
+
+    }
+}
