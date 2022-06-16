777328b8fcd19f030fd343f7e40ee8b8de1074b3
MamedYahyayev <yahyayev2001@gmail.com>
2022-06-16 12:41:37 +0400

    converted created for decrease buttons to make it disable when the value is 0, close #64

---
+++ LookScore/LookScoreManageStatisticsClient/Converter/ButtonActiveConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace LookScoreManageStatisticsClient.Converter
+{
+    public class ButtonActiveConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int statisticValue = (int)value;
+
+            if (statisticValue <= 0) return false;
+
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
