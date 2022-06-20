6c41e90511ab12e0c0c8991cc8315883739c3a7f
MamedYahyayev <yahyayev2001@gmail.com>
2022-06-20 11:13:12 +0400

    games array bind to game list item source

--- LookScore/LookScoreViewerClient/ViewModel/GameListViewModel.cs
+++ LookScore/LookScoreViewerClient/ViewModel/GameListViewModel.cs
@@ -6,14 +6,30 @@ using System.Text;
 using System.Threading.Tasks;
 
 using ReactiveUI;
+using LookScoreServer.Service.WCFServices;
+using LookScoreViewerClient.Contract;
+using System.ServiceModel;
 
 namespace LookScoreViewerClient.ViewModel
 {
     public class GameListViewModel : BaseViewModel
     {
+        #region Private Properties
+
+        private readonly IGameService _gameService;
+
+        #endregion
+
         public GameListViewModel()
         {
-            Games = new Game[25];
+            var gameCallback = new GameCallback();
+
+            InstanceContext gameCallbackInstance = new InstanceContext(gameCallback);
+            DuplexChannelFactory<IGameService> channelFactory = new DuplexChannelFactory<IGameService>(gameCallbackInstance, "GameService");
+            _gameService = channelFactory.CreateChannel();
+            _gameService.JoinToChannel();
+
+            Games = _gameService.FindAllGameDetails();
         }
 
         #region Public Properties
