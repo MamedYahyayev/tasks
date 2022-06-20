54d7347b4764487a40776bbc0d9185b21ac79fbf
MamedYahyayev <yahyayev2001@gmail.com>
2022-06-20 13:16:39 +0400

    path to image converter created

---
+++ LookScore/LookScoreCommon/Converter/PathToImageConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace LookScoreCommon.Converter
+{
+    public class PathToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string path = (string)value;
+            if (path == null || path.Length == 0)
+            {
+                return "";
+            }
+
+            return new BitmapImage(new Uri(path));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
