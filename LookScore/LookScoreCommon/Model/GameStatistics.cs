f95c2831f015083c174b2e04c82ad5d4e5d4b287
MamedYahyayev <yahyayev2001@gmail.com>
2022-06-20 16:26:10 +0400

    game property ignored from json and xml file in game statistics model

--- LookScore/LookScoreCommon/Model/GameStatistics.cs
+++ LookScore/LookScoreCommon/Model/GameStatistics.cs
@@ -1,18 +1,30 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace LookScoreCommon.Model
 {
     [Serializable]
+    [DataContract]
     public class GameStatistics
     {
-        public Statistics HomeClub { get; set; } 
+        [DataMember]
+        public Statistics HomeClub { get; set; }
+
+        [DataMember]
         public Statistics GuestClub { get; set; }
+
+        [JsonIgnore]
+        [XmlIgnore]
+        [DataMember]
         public Game Game { get; set; }
 
+        [DataMember]
         public int GameId { get; set; }
     }
 }
