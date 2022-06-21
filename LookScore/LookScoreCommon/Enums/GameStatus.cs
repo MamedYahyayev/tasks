0ca76aa4b4683a25a96179733a1ccc9cfe765e5c
MamedYahyayev <yahyayev2001@gmail.com>
2022-06-21 12:29:13 +0400

    game status enum added, close #98

---
+++ LookScore/LookScoreCommon/Enums/GameStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LookScoreCommon.Enums
+{
+    public enum GameStatus
+    {
+        UNSTARTED,
+        STARTED,
+        DELAYED,
+        STOPPED,
+        HALF_TIME,
+        FULL_TIME
+    }
+}
