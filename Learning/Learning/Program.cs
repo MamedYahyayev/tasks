f77e4c317074cd2b407a23fcff6b20608f483dd4
MamedYahyayev <yahyayev2001@gmail.com>
2022-06-22 14:23:17 +0400

    project created for learning purposes

---
+++ Learning/Learning/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add(1, 2, 3, 4, 5, 6, 7, 8);
+        }
+
+        public static void Add(params int[] nums)
+        {
+            foreach (int num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
