b5d2ad0d48b767b9f72cf1b2c9dca8e2ad90c9e3
MamedYahyayev <yahyayev2001@gmail.com>
2022-06-23 12:10:14 +0400

    dynamic objects

--- Learning/Learning/Program.cs
+++ Learning/Learning/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -21,6 +23,25 @@ namespace Learning
 
             Console.WriteLine("********Hash********");
             Console.WriteLine(ComputeHash("salam"));
+
+            Console.WriteLine("*****Comparison*****");
+
+            CultureInfo.CurrentCulture = new CultureInfo("tr-TR");
+
+            Console.WriteLine(CompareStrings("SıL", "SIL"));
+
+            Console.WriteLine("\ndynamic and var keyword");
+
+            DynamicAndVarKeywords();
+
+            Console.WriteLine("\ndynamic object");
+
+            dynamic obj = new Dynamic();
+            obj.LastName = "Samirov";
+            Console.WriteLine(obj.LastName);
+
+            obj["FirstName"] = "Samir";
+            Console.WriteLine(obj["FirstName"]);
 
             Console.ReadLine();
         }
@@ -103,12 +124,76 @@ namespace Learning
             // we can hash password with this method
             return result;
         }
+
+        public static bool CompareStrings(string first, string second)
+        {
+            return first.Equals(second, StringComparison.CurrentCultureIgnoreCase);
+        }
+        /// <summary>
+        /// Dynamic keyword will do boxing and unboxing operations behind the scenes
+        /// and we don't need to explicitly box or unbox,
+        /// it is the same as
+        /// object obj; 
+        /// but the difference is if we use object we have to convert values
+        /// </summary>
+        public static void DynamicAndVarKeywords()
+        {
+            dynamic obj = 3.4;
+
+            obj = 2;
+
+            object realObj = 1;
+
+            double doubleWithDynamic = obj; // it will do cast behind the scenes
+            // double doubleWithObj = realObj; // compile error, needs to cast
+
+
+            var num = 1;
+
+            num = (int)1.5;
+        }
     }
 
 
     class Person
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Surname { get; set; }
+    }
+
+    class Student : Person
+    {
+        public Student()
+        {
+            this.Name = "saassa";
+            this.Surname = "sasda";
+        }
+    }
+
+    class Dynamic : DynamicObject
+    {
+        Dictionary<string, object> _dictionary = new Dictionary<string, object>();
+
+        public object this[string propertyName]
+        {
+            get { return _dictionary[propertyName]; }
+            set { AddProperty(propertyName, value); }
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return _dictionary.TryGetValue(binder.Name, out result);
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            AddProperty(binder.Name, value);
+            return true;
+        }
+
+        public void AddProperty(string propertyName, object value)
+        {
+            _dictionary.Add(propertyName, value);
+        }
     }
 }
